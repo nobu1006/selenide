c0bca3fa34325429fc75d57ec614ee98cdb9c942
Nobuhiko Tobita <nobuhiko.tobita@gmail.com>
2022-06-06 16:49:32 +0900

    first commit

---
+++ src/test/java/test/SampleTest.java
@@ -0,0 +1,33 @@
+package test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void test01() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1080","-ignore-certificate-errors");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("http://www.google.com/");
+        System.out.println(driver.getTitle());
+//        driver.quit();
+    }
+}
